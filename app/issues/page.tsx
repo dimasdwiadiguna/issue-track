d1766d68be46ce1f7f2b9a11c2452f755f5e3bf9
Dimas <dimasdwiadiguna@gmail.com>
2025-04-03 12:37:12 +0700

    Initiate Radix UI theme

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { Button } from "@radix-ui/themes";
 
 const IssuesPage = () => {
   return (
     <div>
       <h1>IssuesPage</h1>
+      <Button>Add issues</Button>
     </div>
   );
 };
