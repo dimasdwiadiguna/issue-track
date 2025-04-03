f4d57c8d5604a8b0875dd28ec9d69a38ff50c10e
Dimas <dimasdwiadiguna@gmail.com>
2025-04-03 21:26:34 +0700

    Adding error handling in FE

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,7 +1,9 @@
 "use client";
 
-import React from "react";
-import { TextField, Button } from "@radix-ui/themes";
+import React, { useState } from "react";
+import { TextField, Button, Flex, Callout } from "@radix-ui/themes";
+import { IoMdInformationCircleOutline } from "react-icons/io";
+
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
 import { Controller, useForm } from "react-hook-form";
@@ -16,33 +18,49 @@ interface IssueForm {
 const NewIssuePage = () => {
   const { register, control, handleSubmit, reset } = useForm<IssueForm>();
   const router = useRouter();
+  const [error, setError] = useState("");
 
   return (
-    <form
-      className="max-w-xl mx-auto space-y-3"
-      onSubmit={handleSubmit(async (data) => {
-        await axios.post("/api/issues", data);
-        router.push("/issues");
-      })}
-    >
+    <div className="max-w-xl mx-auto space-y-3">
       <h1>New Issue</h1>
-      <TextField.Root
-        placeholder="Issue title"
-        {...register("title")}
-      ></TextField.Root>
-      <Controller
-        name="description"
-        control={control}
-        render={({ field }) => (
-          <SimpleMDE {...field} placeholder="Type your description here..." />
-        )}
-      />
-
-      <Button type="submit">Submit issue</Button>
-      <Button type="button" onClick={() => reset()}>
-        Reset
-      </Button>
-    </form>
+      {error && (
+        <Callout.Root variant="surface" color="crimson" size="1">
+          <Callout.Icon>
+            <IoMdInformationCircleOutline />
+          </Callout.Icon>
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      )}
+      <form
+        className="space-y-3"
+        onSubmit={handleSubmit(async (data) => {
+          try {
+            await axios.post("/api/issues", data);
+            router.push("/issues");
+          } catch (error) {
+            setError("An unexpected error occured");
+          }
+        })}
+      >
+        <TextField.Root
+          placeholder="Issue title"
+          {...register("title")}
+        ></TextField.Root>
+        <Controller
+          name="description"
+          control={control}
+          render={({ field }) => (
+            <SimpleMDE {...field} placeholder="Type your description here..." />
+          )}
+        />
+        <Flex className="flex gap-1.5">
+          <Button type="submit">Submit issue</Button>
+          <Button type="button" variant="soft" onClick={() => reset()}>
+            Reset
+          </Button>
+        </Flex>
+      </form>
+    </div>
   );
 };
 
