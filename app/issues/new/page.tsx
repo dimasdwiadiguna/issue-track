85df9d957e05f0f702783d4b32fdb816f3af564e
Dimas <dimasdwiadiguna@gmail.com>
2025-04-04 10:52:27 +0700

    Implement client side error handling

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useState } from "react";
-import { TextField, Button, Flex, Callout } from "@radix-ui/themes";
+import { TextField, Button, Flex, Callout, Text } from "@radix-ui/themes";
 import { IoMdInformationCircleOutline } from "react-icons/io";
 
 import SimpleMDE from "react-simplemde-editor";
@@ -9,14 +9,22 @@ import "easymde/dist/easymde.min.css";
 import { Controller, useForm } from "react-hook-form";
 import axios from "axios";
 import { useRouter } from "next/navigation";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { createIssueSchema } from "@/app/validationSchema";
+import { z } from "zod";
 
-interface IssueForm {
-  title: string;
-  description: string;
-}
+type IssueForm = z.infer<typeof createIssueSchema>;
 
 const NewIssuePage = () => {
-  const { register, control, handleSubmit, reset } = useForm<IssueForm>();
+  const {
+    register,
+    control,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<IssueForm>({
+    resolver: zodResolver(createIssueSchema),
+  });
   const router = useRouter();
   const [error, setError] = useState("");
 
@@ -46,6 +54,11 @@ const NewIssuePage = () => {
           placeholder="Issue title"
           {...register("title")}
         ></TextField.Root>
+        {errors.title && (
+          <Text color="red" as="p">
+            {errors.title.message}
+          </Text>
+        )}
         <Controller
           name="description"
           control={control}
@@ -53,6 +66,11 @@ const NewIssuePage = () => {
             <SimpleMDE {...field} placeholder="Type your description here..." />
           )}
         />
+        {errors.description && (
+          <Text color="red" as="p">
+            {errors.description.message}
+          </Text>
+        )}
         <Flex className="flex gap-1.5">
           <Button type="submit">Submit issue</Button>
           <Button type="button" variant="soft" onClick={() => reset()}>
